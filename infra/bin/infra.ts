f4b4d3c6cd517cc267d0931a8e900386dcbc7ebd
Lisabeth Cron <latcron@gmail.com>
2026-06-04 11:39:14 -0700

    Add test and prod stacks to infra.ts for deploy.yml to work

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,18 +3,11 @@ import * as cdk from 'aws-cdk-lib/core';
 import { InfraStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+new InfraStack(app, 'CookbookStack-Test', {
+  env: { account: '558215002534', region: 'us-west-2' },
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new InfraStack(app, 'CookbookStack-Prod', {
+  env: { account: '558215002534', region: 'us-west-2' },
 });
